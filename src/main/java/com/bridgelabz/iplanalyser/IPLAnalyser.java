879baa226ca8b7386ff6ccd6200c8a1f226772f5
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-10-31 21:11:31 +0530

    [Jitendra]Added: Method to get batsman with top strike rate

--- src/main/java/com/bridgelabz/iplanalyser/IPLAnalyser.java
+++ src/main/java/com/bridgelabz/iplanalyser/IPLAnalyser.java
@@ -60,6 +60,23 @@ public class IPLAnalyser {
 		}
 	}
 
+	public String getPlayersWithTopStrikeRate() throws IPLException {
+		try (Writer writer = new FileWriter("./src/test/resources/IPLBattingStrikeRate.json")) {
+			if (IPLRunCSVList == null || IPLRunCSVList.size() == 0) {
+				throw new IPLException("No data", IPLException.ExceptionType.NO_DATA);
+			}
+			Comparator<IPLRuns> iplComparator = Comparator.comparing(census -> census.strikeRate);
+			this.sortInDescendOrder(iplComparator);
+			String json = new Gson().toJson(IPLRunCSVList);
+			Gson gson = new GsonBuilder().create();
+			gson.toJson(IPLRunCSVList, writer);
+			return json;
+
+		} catch (RuntimeException | IOException e) {
+			throw new IPLException(e.getMessage(), IPLException.ExceptionType.FILE_OR_HEADER_PROBLEM);
+		}
+	}
+
 	private void sortInDescendOrder(Comparator<IPLRuns> IPLComparator) {
 		for (int i = 0; i < IPLRunCSVList.size() - 1; i++) {
 			for (int j = 0; j < IPLRunCSVList.size() - i - 1; j++) {
--- src/test/java/com/bridgelabz/iplanalyser/IPLAnalyserTest.java
+++ src/test/java/com/bridgelabz/iplanalyser/IPLAnalyserTest.java
@@ -12,8 +12,8 @@ public class IPLAnalyserTest {
 	@Test
 	public void givenIPLRunCSVFile_ShouldReturnCorrectRecords() {
 		try {
-			IPLAnalyser censusAnalyser = new IPLAnalyser();
-			int numOfRecords = censusAnalyser.loadIPLRunsData(IPL_RUN_CSV_FILE_PATH);
+			IPLAnalyser iplAnalyser = new IPLAnalyser();
+			int numOfRecords = iplAnalyser.loadIPLRunsData(IPL_RUN_CSV_FILE_PATH);
 			Assert.assertEquals(100, numOfRecords);
 		} catch (IPLException e) {
 			e.printStackTrace();
@@ -23,8 +23,8 @@ public class IPLAnalyserTest {
 	@Test
 	public void givenIPLWicketsCSVFile_ShouldReturnCorrectRecords() {
 		try {
-			IPLAnalyser censusAnalyser = new IPLAnalyser();
-			int numOfRecords = censusAnalyser.loadIPLRunsData(IPL_WICKET_CSV_FILE_PATH);
+			IPLAnalyser iplAnalyser = new IPLAnalyser();
+			int numOfRecords = iplAnalyser.loadIPLRunsData(IPL_WICKET_CSV_FILE_PATH);
 			Assert.assertEquals(100, numOfRecords);
 		} catch (IPLException e) {
 			e.printStackTrace();
@@ -34,14 +34,26 @@ public class IPLAnalyserTest {
 	@Test
 	public void givenIPLRunCSVFile_ShouldReturnBatsMan_WithHighestAverageRun() {
 		try {
-			IPLAnalyser iPLAnalyser = new IPLAnalyser();
-			iPLAnalyser.loadIPLRunsData(IPL_RUN_CSV_FILE_PATH);
-			String sortCensusData = iPLAnalyser.getPlayersWithTopAverages();
-			IPLRuns[] iplRuns = new Gson().fromJson(sortCensusData, IPLRuns[].class);
+			IPLAnalyser iplAnalyser = new IPLAnalyser();
+			iplAnalyser.loadIPLRunsData(IPL_RUN_CSV_FILE_PATH);
+			String sortIPLData = iplAnalyser.getPlayersWithTopAverages();
+			IPLRuns[] iplRuns = new Gson().fromJson(sortIPLData, IPLRuns[].class);
 			Assert.assertEquals("MS Dhoni", iplRuns[0].player);
 		} catch (IPLException e) {
 			e.printStackTrace();
 		}
 	}
 
+	@Test
+	public void givenIPLRunCSVFile_ShouldReturnBatsMan_WithHighestStrikeRate() {
+		try {
+			IPLAnalyser iplAnalyser = new IPLAnalyser();
+			iplAnalyser.loadIPLRunsData(IPL_RUN_CSV_FILE_PATH);
+			String sortIPLData = iplAnalyser.getPlayersWithTopStrikeRate();
+			IPLRuns[] iplRuns = new Gson().fromJson(sortIPLData, IPLRuns[].class);
+			Assert.assertEquals("Ishant Sharma", iplRuns[0].player);
+		} catch (IPLException e) {
+			e.printStackTrace();
+		}
+	}
 }
