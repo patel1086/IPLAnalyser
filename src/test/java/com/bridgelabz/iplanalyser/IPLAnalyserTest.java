a4e7362b89f238683cc237a4fc76855dab1951b7
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-10-31 22:00:54 +0530

    [Jitendra]Added: Test case to check who has highest boundaries

--- src/test/java/com/bridgelabz/iplanalyser/IPLAnalyserTest.java
+++ src/test/java/com/bridgelabz/iplanalyser/IPLAnalyserTest.java
@@ -56,4 +56,17 @@ public class IPLAnalyserTest {
 			e.printStackTrace();
 		}
 	}
+
+	@Test
+	public void givenIPLRunCSVFile_ShouldReturnBatsMan_WithHighestBoundaries() {
+		try {
+			IPLAnalyser iplAnalyser = new IPLAnalyser();
+			iplAnalyser.loadIPLRunsData(IPL_RUN_CSV_FILE_PATH);
+			String sortedIPLData = iplAnalyser.getPlayersWithTopBoundaries();
+			IPLRuns[] iplRuns = new Gson().fromJson(sortedIPLData, IPLRuns[].class);
+			Assert.assertEquals("Andre Russell", iplRuns[0].player);
+		} catch (IPLException e) {
+			e.printStackTrace();
+		}
+	}
 }
