1df4ff63d3f230edad9de87f2f7f229a85a717f4
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-10-31 20:53:56 +0530

    [Jitendra]Added: Method to get batsman with top average in descending order

--- src/main/java/com/bridgelabz/iplanalyser/IPLAnalyser.java
+++ src/main/java/com/bridgelabz/iplanalyser/IPLAnalyser.java
@@ -1,12 +1,17 @@
 package com.bridgelabz.iplanalyser;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
@@ -25,7 +30,7 @@ public class IPLAnalyser {
 			throw new IPLException(e.getMessage(), IPLException.ExceptionType.FILE_OR_HEADER_PROBLEM);
 		}
 	}
-	
+
 	public int loadIPLWicketsData(String csvFilePath) throws IPLException {
 		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
 			IPLWicketsCSVList = CSVBuilderFactory.createCSVBuilder().getCSVFList(reader, IPLWickets.class);
@@ -38,4 +43,33 @@ public class IPLAnalyser {
 		}
 	}
 
+	public String getPlayersWithTopAverages() throws IPLException {
+		try (Writer writer = new FileWriter("./src/test/resources/IPLBattingAvg.json")) {
+			if (IPLRunCSVList == null || IPLRunCSVList.size() == 0) {
+				throw new IPLException("No data", IPLException.ExceptionType.NO_DATA);
+			}
+			Comparator<IPLRuns> IPLComparator = Comparator.comparing(census -> census.avg);
+			this.sortInDesendOrder(IPLComparator);
+			String json = new Gson().toJson(IPLRunCSVList);
+			Gson gson = new GsonBuilder().create();
+			gson.toJson(IPLRunCSVList, writer);
+			return json;
+
+		} catch (RuntimeException | IOException e) {
+			throw new IPLException(e.getMessage(), IPLException.ExceptionType.FILE_OR_HEADER_PROBLEM);
+		}
+	}
+
+	private void sortInDesendOrder(Comparator<IPLRuns> IPLComparator) {
+		for (int i = 0; i < IPLRunCSVList.size() - 1; i++) {
+			for (int j = 0; j < IPLRunCSVList.size() - i - 1; j++) {
+				IPLRuns census1 = IPLRunCSVList.get(j);
+				IPLRuns census2 = IPLRunCSVList.get(j + 1);
+				if (IPLComparator.compare(census1, census2) < 0) {
+					IPLRunCSVList.set(j, census2);
+					IPLRunCSVList.set(j + 1, census1);
+				}
+			}
+		}
+	}
 }
--- src/test/java/com/bridgelabz/iplanalyser/IPLAnalyserTest.java
+++ src/test/java/com/bridgelabz/iplanalyser/IPLAnalyserTest.java
@@ -3,6 +3,8 @@ package com.bridgelabz.iplanalyser;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.google.gson.Gson;
+
 public class IPLAnalyserTest {
 	private static final String IPL_RUN_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
 	private static final String IPL_WICKET_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostWkts.csv";
@@ -24,6 +26,19 @@ public class IPLAnalyserTest {
 			IPLAnalyser censusAnalyser = new IPLAnalyser();
 			int numOfRecords = censusAnalyser.loadIPLRunsData(IPL_WICKET_CSV_FILE_PATH);
 			Assert.assertEquals(100, numOfRecords);
+		} catch (IPLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void givenIPLRunCSVFile_ShouldReturnBatsMan_WithHighestAverageRun() {
+		try {
+			IPLAnalyser iPLAnalyser = new IPLAnalyser();
+			iPLAnalyser.loadIPLRunsData(IPL_RUN_CSV_FILE_PATH);
+			String sortCensusData = iPLAnalyser.getPlayersWithTopAverages();
+			IPLRuns[] iplRuns = new Gson().fromJson(sortCensusData, IPLRuns[].class);
+			Assert.assertEquals("MS Dhoni", iplRuns[0].player);
 		} catch (IPLException e) {
 			e.printStackTrace();
 		}
